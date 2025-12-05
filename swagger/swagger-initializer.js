4905c92a731ed80dbd509abec869de0de8fe6171
fstez <bsergachev@gmail.com>
2025-12-05 14:49:36 +0200

    Changed openapi.yaml and added swagger

---
+++ swagger/swagger-initializer.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  window.ui = SwaggerUIBundle({
+    url: "../api/openapi.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout"
+  });
+};
+
